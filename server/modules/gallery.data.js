ff19e8694edaefab467c848591f665e2c3dbab0a
Diego Baeza <diegob12@live.com>
2022-07-09 17:25:17 -0500

    update images in server data, rearrange return portion of function app

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    
+    { id: 1, path: 'images/cat.jpg', description: 'Photo of a cat judging my code.', likes: 0 },
+    { id: 2, path: 'images/leaningTower.jpg', description: 'Photo of a leaning Tower.', likes: 0 },
+    { id: 3, path: 'images/meme.jpg', description: 'Photo of a meme.', likes: 0 },
+    { id: 4, path: 'images/rainForest.jpg', description: 'Photo of a rain forest.', likes: 0 },
+    { id: 5, path: 'images/motivationalQuote.jpg', description: 'Photo of a motivational quote.', likes: 0 },
+    { id: 6, path: 'images/purpleSky.jpg', description: 'Photo of the entrance into Animal Kingdom in Florida, USA.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <div>
+        <h2>Gallery goes here</h2>
+        
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
