0ad32ace6516b18c860752db9e639de9e217a1c7
Diego Baeza <diegob12@live.com>
2022-07-10 14:12:50 -0500

    add functionality that allows users to click on photo and swap the photo with a description of said photo

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,13 +1,23 @@
+import {useState} from 'react';
+
 function handleGalleryItem({item, updateItem}) {
   
+    let [state, setState] = useState(false)
+
     const likeItem = () => {
         updateItem(item.id)
     }
+
+    const showDesc = () => {
+        console.log('picture clicked')
+        setState(true)
+    }
   
+    console.log(state);
     return (
     <>
       <div key={item.id}>
-        <img src={item.path} />
+        {state ? `${item.description}`: <img onClick = {showDesc} src={item.path} />}
         <button onClick={likeItem}>love it!</button>
         <p>This item has {item.likes} likes</p>
       </div>
